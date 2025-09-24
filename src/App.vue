bafb73d8bcac55c47cb98e009e5fd3b9a2a544f0
Avalanchy <avalanchy@AvalanchydeMacBook-Air-4.local>
2025-09-24 19:17:10 -0400

    test deploy

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
 import { RouterView } from 'vue-router'
 import requests from "@/utils/requests";
 
-// requests({
-//   method: "GET",
-//   url: "/front/ad/getAdList"
-// }).then(res => {
-//   console.log(res)
-// })
 
 </script>
 
